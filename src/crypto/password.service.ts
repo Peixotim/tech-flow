cddafad540aef1b99e6a62e80d8c6d740f5bdd74
Peixotim <pedropeixotovz@gmail.com>
2025-12-05 16:28:21 -0300

    feat: Adiciona importação de módulo no PasswordService

--- src/crypto/password.service.ts
+++ src/crypto/password.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as argon2 from 'argon2';
+
 @Injectable()
 export class PasswordService {
   public async hash(password: string): Promise<string> {
