a88a44c5f8f9f82dca4789f2855d75ce3baaeb2c
Peixotim <pedropeixotovz@gmail.com>
2025-12-05 14:45:34 -0300

    feat: Adiciona serviço de hash e verificação de senha com argon2

---
+++ src/crypto/password.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+@Injectable()
+export class PasswordService {
+  public async hash(password: string): Promise<string> {
+    return argon2.hash(password, {
+      type: argon2.argon2id,
+      memoryCost: 2 ** 16, //64 MB
+      timeCost: 3,
+      parallelism: 1,
+    });
+  }
+
+  public async verify(hash: string, password: string): Promise<boolean> {
+    return argon2.verify(hash, password);
+  }
+}
