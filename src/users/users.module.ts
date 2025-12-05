c4c034d3ebe32ec7eabd8a71f857b36321bd42e3
Peixotim <pedropeixotovz@gmail.com>
2025-12-05 16:28:27 -0300

    feat: Substitui PasswordService por CryptoModule no módulo Users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './entity/users.entity';
-import { PasswordService } from 'src/crypto/password.service';
+import { CryptoModule } from 'src/crypto/crypto.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersEntity]), PasswordService],
+  imports: [TypeOrmModule.forFeature([UsersEntity]), CryptoModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
