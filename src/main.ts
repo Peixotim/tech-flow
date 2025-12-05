9807fc82a3791a74e5cb86308c38c1a077f06c69
Peixotim <pedropeixotovz@gmail.com>
2025-12-05 11:19:50 -0300

    feat: Adiciona configuração de logger e inicialização do banco de dados no bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger as PinoLogger } from 'nestjs-pino'; // Renomeie para não confundir
+import { Logger as NestLogger } from '@nestjs/common'; // Importe o nativo
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  if (process.env.NODE_ENV === 'production') {
+    app.useLogger(app.get(PinoLogger));
+  } else {
+    app.useLogger(new NestLogger());
+  }
+
+  const logger =
+    process.env.NODE_ENV === 'production'
+      ? app.get(PinoLogger)
+      : new NestLogger('Bootstrap');
+
+  const dataSource = app.get(DataSource);
+
+  if (dataSource.isInitialized) {
+    logger.log('Database: Connected and Ready to Use!');
+  } else {
+    logger.error('Critical Error: The Database did not connect!');
+    process.exit(1);
+  }
+
+  await app.listen(process.env.PORT ?? 8080);
+
+  logger.log(`Application running on: ${await app.getUrl()}`);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
