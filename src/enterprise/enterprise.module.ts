de5cafb5c10e7acbc25086a32709bdec50f80c9d
Peixotim <pedropeixotovz@gmail.com>
2025-12-08 17:14:23 -0300

    feat: Adiciona controllers e providers ao módulo Enterprise

--- src/enterprise/enterprise.module.ts
+++ src/enterprise/enterprise.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EnterpriseService } from './enterprise.service';
+import { EnterpriseController } from './enterprise.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnterpriseEntity } from './enterprise/enterprise.entity';
 
 @Module({
-  controllers: [],
-  impor,
+  imports: [TypeOrmModule.forFeature([EnterpriseEntity])],
+  controllers: [EnterpriseController],
+  providers: [EnterpriseService],
 })
 export class EnterpriseModule {}
