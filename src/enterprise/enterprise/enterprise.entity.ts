7431553d95315bd7e568aadf0525f0157f8fbd60
Peixotim <pedropeixotovz@gmail.com>
2025-12-09 12:39:40 -0300

    feat: Cria entidade Enterprise com propriedades e relacionamentos

--- src/enterprise/enterprise/enterprise.entity.ts
+++ src/enterprise/enterprise/enterprise.entity.ts
@@ -1,4 +1,48 @@
-import { Entity } from 'typeorm';
+import { UsersEntity } from 'src/users/entity/users.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-@Entity()
-export class EnterpriseEntity {}
+@Entity({ name: 'enterprise' })
+export class EnterpriseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Index()
+  @Column({ unique: true, length: 50 })
+  slug: string; //Vai servir como se fosse um nickname (exemplo : Nome(Escola Estadual Antônio Silva , no slug fica EEAS))
+
+  @Column({ nullable: false, length: 18, unique: true })
+  cnpj: string;
+
+  @Column({ name: 'logo_url', nullable: true })
+  logoUrl: string;
+
+  @Column({ name: 'primary_color', default: '#000000', length: 7 })
+  primaryColor: string;
+
+  @Column({ default: true, name: 'is_active' })
+  isActive: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+
+  @OneToMany(() => UsersEntity, (users) => users.enterprise)
+  users: UsersEntity[];
+}
