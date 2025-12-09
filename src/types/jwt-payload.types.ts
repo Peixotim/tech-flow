d0ab405e0f483a333abe6ef05ef12d2613b6a983
Peixotim <pedropeixotovz@gmail.com>
2025-12-09 12:39:52 -0300

    feat: Reorganiza a interface JwtPayload para melhor legibilidade

--- src/types/jwt-payload.types.ts
+++ src/types/jwt-payload.types.ts
@@ -1,11 +1,15 @@
+import { UserRoles } from 'src/users/enum/roles.enum';
+
 export interface JwtPayload {
   sub: string; //UUID
   iss?: string; // Issuer
   aud?: string | string[]; // Servico que vai servir o jwt
   jti?: string; // Unique Token ID (Para revogação)
+  iat?: number; //Issued At
+  exp?: number; //Expiration
 
   email: string;
 
-  iat?: number; //Issued At
-  exp?: number; //Expiration
+  role: UserRoles;
+  enterprise: string | null;
 }
