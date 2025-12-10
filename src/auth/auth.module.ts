e5a7051bce0ca026e0658411623e0c41b7be852e
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 10:58:00 -0300

    feat: Adiciona JwtStrategy e PassportModule ao AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,15 @@ import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CryptoModule } from 'src/crypto/crypto.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     UsersModule,
     CryptoModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       global: true,
