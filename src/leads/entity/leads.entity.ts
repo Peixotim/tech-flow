94a8c768d4569bdd17d423483f98d6c2f3fa5ec1
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 09:27:58 -0300

    feat: Implementa a entidade Leads com propriedades e relacionamento com Enterprise

--- src/leads/entity/leads.entity.ts
+++ src/leads/entity/leads.entity.ts
@@ -1,4 +1,20 @@
-import { Entity } from 'typeorm';
+import { EnterpriseEntity } from 'src/enterprise/enterprise/enterprise.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'leads' })
-export class LeadsEntity {}
+export class LeadsEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 80, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 254, nullable: true })
+  email?: string;
+
+  @Column({ type: 'varchar', length: 18 })
+  number: string;
+
+  @ManyToOne(() => EnterpriseEntity, (enterprise) => enterprise.leads)
+  enterprise: EnterpriseEntity;
+}
