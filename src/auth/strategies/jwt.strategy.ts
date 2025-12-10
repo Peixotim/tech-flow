72cb0146a90c110c405c3a085069d84fb57b076c
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 10:58:17 -0300

    feat: Adiciona a estratégia JwtStrategy para autenticação baseada em JWT

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+interface JwtPayload {
+  sub: string;
+  email: string;
+  role: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'DEV_SECRET_KEY',
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return {
+      uuid: payload.sub,
+      email: payload.email,
+      role: payload.role,
+    };
+  }
+}
