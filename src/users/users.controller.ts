35dd365fa6ed304a0c60c9f1c08d93dc9307c5b5
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 10:58:24 -0300

    feat: Adiciona método para criação de usuário mestre com autenticação e controle de roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
+import { UsersResponseMasterDTO } from './DTOs/use-master-create-response.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRoles } from './enum/roles.enum';
+import { UsersMasterCreateDTO } from './DTOs/users-master-create.dto';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('master')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRoles.MASTER)
+  public async createUserMaster(
+    @Body() requestCreate: UsersMasterCreateDTO,
+  ): Promise<UsersResponseMasterDTO> {
+    return await this.usersService.createMaster(requestCreate);
+  }
+}
