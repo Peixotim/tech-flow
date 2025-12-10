64393065c504619a714d8a1fe99fbba39857e75e
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 10:58:27 -0300

    feat: Adiciona método para criação de usuário mestre com validações e tratamento de erros

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,6 +13,9 @@ import { UserCreateDTO } from './DTOs/users-create.dto';
 import { PasswordService } from '../crypto/password.service';
 import { UsersResponseDTO } from './DTOs/user-create-response.dto';
 import { EnterpriseService } from 'src/enterprise/enterprise.service';
+import { UsersResponseMasterDTO } from './DTOs/use-master-create-response.dto';
+import { UsersMasterCreateDTO } from './DTOs/users-master-create.dto';
+import { UserRoles } from './enum/roles.enum';
 
 @Injectable()
 export class UsersService {
@@ -90,6 +93,80 @@ export class UsersService {
 
       this.logger.error({
         message: 'Critical error during user creation process',
+        method: 'createUser',
+        payload: payloadSafe,
+        originalError: errorMessage,
+        errorCode: errorCode,
+        stack: error instanceof Error ? error.stack : null,
+      });
+
+      if (errorCode === '23505') {
+        throw new ConflictException(
+          'Email already registered (concurrency detected).',
+        );
+      }
+
+      throw new InternalServerErrorException(
+        'Unexpected error while creating user. Please try again later or contact support.',
+      );
+    }
+  }
+
+  public async createMaster(
+    requestCreate: UsersMasterCreateDTO,
+  ): Promise<UsersResponseMasterDTO> {
+    if (!requestCreate || Object.keys(requestCreate).length === 0) {
+      throw new BadRequestException('Error: request payload is empty.');
+    }
+
+    const userExists = await this.usersRepository.exists({
+      where: { email: requestCreate.email },
+    });
+
+    if (userExists) {
+      throw new ConflictException(
+        'Error already has a user registered with this email!',
+      );
+    }
+    try {
+      const passwordHashed = await this.passwordService.hash(
+        requestCreate.password,
+      );
+
+      const role: UserRoles = UserRoles.MASTER;
+
+      const userCreate: UsersEntity = this.usersRepository.create({
+        name: requestCreate.name,
+        email: requestCreate.email,
+        password: passwordHashed,
+        role,
+      });
+
+      const userSaved: UsersEntity =
+        await this.usersRepository.save(userCreate);
+
+      const responseUser: UsersResponseMasterDTO = {
+        uuid: userSaved.uuid,
+        name: userSaved.name,
+        email: userSaved.email,
+        createdAt: userSaved.createdAt,
+        role: userSaved.role,
+      };
+
+      return responseUser;
+    } catch (error: unknown) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...payloadSafe } = requestCreate;
+
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      const errorCode =
+        error instanceof Object && 'code' in error
+          ? (error as Record<string, unknown>).code
+          : null;
+
+      this.logger.error({
+        message: 'Critical error during master creation process',
         method: 'createUser',
         payload: payloadSafe,
         originalError: errorMessage,
