5bf94763a276da9bca3975241a0d6cb7a30e60ce
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 09:28:08 -0300

    feat: Corrige o nome da classe de UserResponseDTO para UsersResponseDTO e atualiza exemplo do nome

--- src/users/DTOs/user-create-response.dto.ts
+++ src/users/DTOs/user-create-response.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { UserRoles } from '../enum/roles.enum';
-export class UserResponseDTO {
+export class UsersResponseDTO {
   @ApiProperty({
     description: 'ID único do usuário',
     example: '123e4567-e89b-12d3-a456-426614174000',
   })
   uuid: string;
 
-  @ApiProperty({ example: 'Pedro de Almeida' })
+  @ApiProperty({ example: 'Pedro Peixoto' })
   name: string;
 
   @ApiProperty({ example: 'pedro@empresa.com' })
