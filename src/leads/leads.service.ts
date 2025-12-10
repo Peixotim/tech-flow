5b5c4620c2029f60750fae8f1622edef9a5d6320
Peixotim <pedropeixotovz@gmail.com>
2025-12-10 14:54:07 -0300

    feat: Adiciona métodos para recuperação de leads por empresa e validação de token JWT

--- src/leads/leads.service.ts
+++ src/leads/leads.service.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
   Logger,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { LeadsResponseDTO } from './DTOs/leads-create-response.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,6 +12,9 @@ import { LeadsEntity } from './entity/leads.entity';
 import { Repository } from 'typeorm';
 import { LeadsCreateDTO } from './DTOs/leads-create.dto';
 import { EnterpriseService } from 'src/enterprise/enterprise.service';
+import { EnterpriseEntity } from 'src/enterprise/enterprise/enterprise.entity';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/types/jwt-payload.types';
 
 @Injectable()
 export class LeadsService {
@@ -18,6 +22,7 @@ export class LeadsService {
     @InjectRepository(LeadsEntity)
     private readonly leadsRepository: Repository<LeadsEntity>,
     private readonly enterpriseService: EnterpriseService,
+    private jwtService: JwtService,
   ) {}
 
   private readonly logger = new Logger(LeadsService.name);
@@ -76,6 +81,65 @@ export class LeadsService {
       throw new InternalServerErrorException(
         'Unexpected error while creating lead. Please try again later or contact support.',
       );
+    }
+  }
+
+  public async getAll(): Promise<LeadsEntity[] | null> {
+    const leads: LeadsEntity[] = await this.leadsRepository.find();
+    if (!leads) {
+      return null;
+    }
+    return leads;
+  }
+
+  public async getLeadEnterprise(enterpriseId: string): Promise<LeadsEntity[]> {
+    const enterprise: EnterpriseEntity | null =
+      await this.enterpriseService.findByUuid(enterpriseId);
+    if (!enterprise) {
+      throw new NotFoundException('Error: No company was found with this uuid');
+    }
+
+    const leads: LeadsEntity[] = await this.leadsRepository.find({
+      where: { enterprise: { uuid: enterpriseId } },
+      relations: ['enterprise'],
+    });
+
+    if (!leads || leads.length === 0) {
+      throw new NotFoundException(
+        'Error, no leads were found for this company!',
+      );
+    }
+    return leads;
+  }
+
+  public async getLeadsEnterpriseToken(
+    access_token: string,
+  ): Promise<LeadsEntity[]> {
+    const token = access_token.replace('Bearer ', '').trim();
+    try {
+      const payload: JwtPayload = this.jwtService.verify(token);
+
+      if (!payload) {
+        throw new UnauthorizedException('Invalid token format.');
+      }
+
+      if (!payload.enterprise) {
+        throw new UnauthorizedException(
+          'User is not associated with any Enterprise.',
+        );
+      }
+
+      return await this.getLeadEnterprise(payload.enterprise);
+    } catch (error) {
+      if (
+        error instanceof UnauthorizedException ||
+        error instanceof NotFoundException
+      ) {
+        throw error;
+      }
+
+      this.logger.error(`Error decoding token: ${error}`);
+      throw new UnauthorizedException('Failed to process token');
     }
   }
 }
