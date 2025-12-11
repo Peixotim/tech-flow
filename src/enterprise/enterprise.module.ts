7b1f2f8d99f0aec0f3df72b581fd593462c4611a
Peixotim <pedropeixotovz@gmail.com>
2025-12-11 12:03:15 -0300

    feat: Adiciona módulos Users e Crypto ao módulo de Enterprise

--- src/enterprise/enterprise.module.ts
+++ src/enterprise/enterprise.module.ts
@@ -3,9 +3,15 @@ import { EnterpriseService } from './enterprise.service';
 import { EnterpriseController } from './enterprise.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnterpriseEntity } from './enterprise/enterprise.entity';
+import { UsersModule } from 'src/users/users.module';
+import { CryptoModule } from 'src/crypto/crypto.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EnterpriseEntity])],
+  imports: [
+    TypeOrmModule.forFeature([EnterpriseEntity]),
+    UsersModule,
+    CryptoModule,
+  ],
   controllers: [EnterpriseController],
   providers: [EnterpriseService],
   exports: [EnterpriseService],
