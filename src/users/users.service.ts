e9e3b1c20b6385170c9cb1252b2c5dfe33ba2fc2
Peixotim <pedropeixotovz@gmail.com>
2025-12-11 12:05:05 -0300

    feat: Adiciona método para criação de usuário administrador com validações

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,6 +17,7 @@ import { UsersResponseMasterDTO } from './DTOs/use-master-create-response.dto';
 import { UsersMasterCreateDTO } from './DTOs/users-master-create.dto';
 import { UserRoles } from './enum/roles.enum';
 import { UsersModifyDTO } from './DTOs/users-modify.dto';
+import { UserCreateAdminDTO } from './DTOs/users-create-admin.dto';
 
 @Injectable()
 export class UsersService {
@@ -187,6 +188,89 @@ export class UsersService {
     }
   }
 
+  public async createUserAdmin(
+    requestCreate: UserCreateAdminDTO,
+  ): Promise<UsersResponseDTO> {
+    if (!requestCreate || Object.keys(requestCreate).length === 0) {
+      throw new BadRequestException('Error: request payload is empty.');
+    }
+
+    const userExists = await this.usersRepository.exists({
+      where: { email: requestCreate.email },
+    });
+
+    if (userExists) {
+      throw new ConflictException(
+        'Error already has a user registered with this email!',
+      );
+    }
+
+    const enterprise = await this.enterpriseService.findByUuid(
+      requestCreate.enterpriseId,
+    );
+
+    if (!enterprise) {
+      throw new NotFoundException(
+        'Error, the company with this uuid does not exist!',
+      );
+    }
+    try {
+      const passwordHashed = await this.passwordService.hash(
+        requestCreate.password,
+      );
+
+      const userCreate: UsersEntity = this.usersRepository.create({
+        name: requestCreate.name,
+        email: requestCreate.email,
+        password: passwordHashed,
+        role: UserRoles.CLIENT_ADMIN,
+        enterprise: enterprise,
+      });
+
+      const userSaved: UsersEntity =
+        await this.usersRepository.save(userCreate);
+
+      const responseUser: UsersResponseDTO = {
+        uuid: userSaved.uuid,
+        name: userSaved.name,
+        email: userSaved.email,
+        createdAt: userSaved.createdAt,
+        role: userSaved.role,
+        enterpriseId: enterprise.uuid,
+      };
+
+      return responseUser;
+    } catch (error: unknown) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...payloadSafe } = requestCreate;
+
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      const errorCode =
+        error instanceof Object && 'code' in error
+          ? (error as Record<string, unknown>).code
+          : null;
+
+      this.logger.error({
+        message: 'Critical error during user creation process',
+        method: 'createUser',
+        payload: payloadSafe,
+        originalError: errorMessage,
+        errorCode: errorCode,
+        stack: error instanceof Error ? error.stack : null,
+      });
+
+      if (errorCode === '23505') {
+        throw new ConflictException(
+          'Email already registered (concurrency detected).',
+        );
+      }
+
+      throw new InternalServerErrorException(
+        'Unexpected error while creating user. Please try again later or contact support.',
+      );
+    }
+  }
   public async findByMail(email: string): Promise<UsersEntity | null> {
     return await this.usersRepository.findOne({
       where: { email },
