b8661a54c8dcae662f05d7f1321341c6d4013308
Peixotim <pedropeixotovz@gmail.com>
2025-12-11 12:04:50 -0300

    feat: Adiciona método para criar empresa e usuário simultaneamente

--- src/enterprise/enterprise.service.ts
+++ src/enterprise/enterprise.service.ts
@@ -6,11 +6,17 @@ import {
   InternalServerErrorException,
   Logger,
 } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { EnterpriseEntity } from './enterprise/enterprise.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EnterpriseCreateDTO } from './DTOs/enterprise-create.dto';
 import { EnterpriseResponseDTO } from './DTOs/enterprise-reponse.dto';
+import { UsersService } from 'src/users/users.service';
+import { EnterpriseCreateAndUserDTO } from './DTOs/enterprise-user-create.dto';
+import { EnterpriseResponseCreateAndUser } from './DTOs/enterprise-user-response.dto';
+import { PasswordService } from 'src/crypto/password.service';
+import { UserRoles } from 'src/users/enum/roles.enum';
+import { UsersEntity } from 'src/users/entity/users.entity';
 
 @Injectable()
 export class EnterpriseService {
@@ -19,6 +25,9 @@ export class EnterpriseService {
   constructor(
     @InjectRepository(EnterpriseEntity)
     private readonly enterpriseRepository: Repository<EnterpriseEntity>,
+    private readonly usersService: UsersService,
+    private readonly dataSource: DataSource,
+    private readonly passwordService: PasswordService,
   ) {}
 
   public async createEnterprise(
@@ -137,5 +146,102 @@ export class EnterpriseService {
     }
 
     return enterprise;
+  }
+
+  public async enterpriseAndUser(
+    requestCreate: EnterpriseCreateAndUserDTO,
+  ): Promise<EnterpriseResponseCreateAndUser> {
+    if (!requestCreate || Object.keys(requestCreate).length === 0) {
+      throw new BadRequestException('Error: request payload is empty.');
+    }
+
+    const [existsEmail, existsCnpj, existsSlug] = await Promise.all([
+      this.usersService.findByMail(requestCreate.email),
+      this.findByCnpj(requestCreate.cnpj),
+      this.findBySlug(requestCreate.slug),
+    ]);
+
+    if (existsEmail)
+      throw new ConflictException(
+        'Error: A user is already registered with this email!',
+      );
+    if (existsCnpj)
+      throw new ConflictException(
+        'Error: An enterprise is already registered with this CNPJ!',
+      );
+    if (existsSlug)
+      throw new ConflictException(
+        'Error: An enterprise is already registered with this Slug!',
+      );
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const newEnterprise = queryRunner.manager.create(EnterpriseEntity, {
+        name: requestCreate.name,
+        slug: requestCreate.slug,
+        cnpj: requestCreate.cnpj,
+        primaryColor: requestCreate.primaryColor,
+        isActive: requestCreate.isActive,
+        logoUrl: requestCreate.logoUrl,
+      });
+
+      const savedEnterprise = await queryRunner.manager.save(newEnterprise);
+
+      const passwordHash = await this.passwordService.hash(
+        requestCreate.password,
+      );
+      const newUser = queryRunner.manager.create(UsersEntity, {
+        email: requestCreate.email,
+        name: requestCreate.fullerName,
+        password: passwordHash,
+        role: UserRoles.CLIENT_ADMIN,
+        enterprise: savedEnterprise,
+      });
+
+      const savedUser = await queryRunner.manager.save(newUser);
+
+      await queryRunner.commitTransaction();
+
+      const response: EnterpriseResponseCreateAndUser = {
+        userUuid: savedUser.uuid,
+        userName: savedUser.name,
+        email: savedUser.email,
+        createdAt: savedUser.createdAt,
+
+        enterpriseUuid: savedEnterprise.uuid,
+        enterpriseName: savedEnterprise.name,
+        slug: savedEnterprise.slug,
+        cnpj: savedEnterprise.cnpj,
+        logoUrl: savedEnterprise.logoUrl,
+        primaryColor: savedEnterprise.primaryColor,
+        isActive: savedEnterprise.isActive,
+      };
+
+      return response;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      const errorStack = error instanceof Error ? error.stack : undefined;
+
+      this.logger.error(
+        `Transaction failed for onboarding: ${errorMessage}`,
+        errorStack,
+      );
+
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(
+        'Unexpected error during enterprise and user creation. Transaction rolled back.',
+      );
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
