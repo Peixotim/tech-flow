746b9c9dc42ae5c4757829261381199cdb7eb944
Peixotim <pedropeixotovz@gmail.com>
2025-12-16 09:24:01 -0300

    feat: Adiciona LeadHistoryEntity ao módulo de leads

--- src/leads/leads.module.ts
+++ src/leads/leads.module.ts
@@ -4,11 +4,15 @@ import { LeadsController } from './leads.controller';
 import { LeadsService } from './leads.service';
 import { LeadsEntity } from './entity/leads.entity';
 import { EnterpriseModule } from 'src/enterprise/enterprise.module';
+import { LeadHistoryEntity } from './entity/lead-history.entity';
 
 @Module({
   controllers: [LeadsController],
   providers: [LeadsService],
-  imports: [TypeOrmModule.forFeature([LeadsEntity]), EnterpriseModule],
+  imports: [
+    TypeOrmModule.forFeature([LeadsEntity, LeadHistoryEntity]),
+    EnterpriseModule,
+  ],
   exports: [LeadsService],
 })
 export class LeadsModule {}
