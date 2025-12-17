17d7eaafdf7985e30c607341b4d94dfb66e43907
Peixotim <pedropeixotovz@gmail.com>
2025-12-17 11:07:34 -0300

    feat: Adiciona logger para monitoramento de acesso no RolesGuard

--- src/auth/guards/roles.guards.ts
+++ src/auth/guards/roles.guards.ts
@@ -3,6 +3,7 @@ import {
   CanActivate,
   ExecutionContext,
   ForbiddenException,
+  Logger,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
@@ -14,6 +15,7 @@ interface AuthenticatedRequest extends Request {
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+  logger = new Logger(RolesGuard.name);
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
@@ -21,13 +23,18 @@ export class RolesGuard implements CanActivate {
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
     );
-
-    const { user } = context.switchToHttp().getRequest<AuthenticatedRequest>();
-
     if (!requiredRoles) {
       return true;
     }
 
+    const { user } = context.switchToHttp().getRequest<AuthenticatedRequest>();
+
+    if (!user) {
+      this.logger.warn(
+        'Usuário não encontrado no request (JwtAuthGuard falhou ou não foi usado?)',
+      );
+      throw new ForbiddenException('Acesso negado: Usuário não identificado.');
+    }
     if (!user || !requiredRoles.includes(user.role)) {
       throw new ForbiddenException(
         'Acesso Negado: Você não tem permissão de Master.',
