3f33a4a66bf5c3c83c09d496a1f5bba953bc1912
Peixotim <pedropeixotovz@gmail.com>
2025-12-17 11:08:23 -0300

    feat: Adiciona enumeração LeadOrigin para origens de leads

---
+++ src/leads/enums/lead-origin.enum.ts
@@ -0,0 +1,12 @@
+export enum LeadOrigin {
+  LINKEDIN = 'linkedin',
+  INSTAGRAM = 'instagram',
+  YOUTUBE = 'youtube',
+  GOOGLE_ORGANIC = 'google_organic',
+  GOOGLE_ADS = 'google_ads',
+  FACEBOOK = 'facebook',
+  INDICATION = 'indication',
+  WEBSITE = 'website',
+  API = 'API',
+  OTHER = 'other',
+}
