846a89e9b36c700753640a24301d891ba46026fd
Peixotim <pedropeixotovz@gmail.com>
2025-12-18 11:16:38 -0300

    feat: Adiciona módulo Playbook como dependência do módulo Enterprise

--- src/enterprise/enterprise.module.ts
+++ src/enterprise/enterprise.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnterpriseEntity } from './enterprise/enterprise.entity';
 import { UsersModule } from 'src/users/users.module';
 import { CryptoModule } from 'src/crypto/crypto.module';
+import { PlaybookModule } from 'src/playbook/playbook.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([EnterpriseEntity]),
     forwardRef(() => UsersModule),
+    forwardRef(() => PlaybookModule),
     CryptoModule,
   ],
   controllers: [EnterpriseController],
